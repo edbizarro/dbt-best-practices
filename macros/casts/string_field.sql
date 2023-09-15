a8b5936a5399ccf592ad53b3f9ba26791ef0e2f8
Bizarro <eduardo.bizarro@meliuz.com.br>
2023-09-15 13:54:49 -0300

    string macro

---
+++ macros/casts/string_field.sql
@@ -0,0 +1,17 @@
+{%- macro string_field(field, cast_type='string', case_null='(N/A)', uppercase=True) -%}
+
+{% set null_value = var('global.value_not_set') %}
+{% if uppercase %}
+coalesce(
+    upper(nullif(nullif(trim({{ safe_cast(field, cast_type) }}), '' ), 'NULL')),
+    '{{ case_null }}'
+)
+
+{% else %}
+coalesce(
+    nullif(nullif(trim({{ safe_cast(field, cast_type) }}), '' ), 'NULL'),
+    '{{ case_null }}'
+)
+{% endif %}
+
+{%- endmacro -%}
